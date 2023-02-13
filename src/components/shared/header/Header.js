bff921760c6b15505b615f6304788e5ac01d29f4
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-13 21:18:45 +0530

    [FIX] New way aroung Navlink

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -11,11 +11,19 @@ const Header = () => {
   color: "orange",
   fontWeight: "bold"                
 }
+const navStyles = ({isActive}) => {
+     return {
+      textDecoration: isActive?"underline": "none",
+      color: isActive?"orange": "white",
+      fontWeight: isActive?"bold": "lighter",  
+     } 
+
+}
 
   return (
     <div>
     <ul>
-     <li><NavLink to="/"  style={({isActive})=> isActive? active: null}>Home</NavLink></li>
+     <li><NavLink to="/"  style={navStyles}>Home</NavLink></li>
      <li><NavLink to="/employees"  style={({isActive})=> isActive? active: null}>Employees</NavLink></li>
      <li><NavLink to="/about"  style={({isActive})=> isActive? active: null}>About</NavLink></li>
      <li><NavLink to="/contact"  style={({isActive})=> isActive? active: null}>Contact</NavLink></li>
