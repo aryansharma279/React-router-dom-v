67d57af950d5bc1fe38fc1393ea3777a0ff39848
Aryan Sharma <developer.aryansharma@gmail.com>
2023-02-13 20:56:42 +0530

    [NEW] Added NavLink in Header and router

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React,{useState} from 'react'
+import { Link,NavLink } from 'react-router-dom'
 import "./Header.css"
 
+
+
 const Header = () => {
+ const active = 
+ { 
+  textDecoration: "underline",
+  color: "orange",
+  fontWeight: "bold"                
+}
+
   return (
     <div>
     <ul>
-     <Link to="/"><li>Home</li></Link>
-     <Link to="/employees"><li>Employees</li></Link>
-     <Link to="/about"><li>About</li></Link>
-     <Link to="/contact"><li>Contact</li></Link>
+     <li><NavLink to="/"  style={({isActive})=> isActive? active: null}>Home</NavLink></li>
+     <li><NavLink to="/employees"  style={({isActive})=> isActive? active: null}>Employees</NavLink></li>
+     <li><NavLink to="/about"  style={({isActive})=> isActive? active: null}>About</NavLink></li>
+     <li><NavLink to="/contact"  style={({isActive})=> isActive? active: null}>Contact</NavLink></li>
      </ul>
-    </div>
+    </div>  
   )
 }
 
